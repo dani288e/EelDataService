a8d9d6ec6205d416ec4ac37caa1c5fede3c8e4af
dani288e <daniel_holm@live.dk>
2017-02-12 13:07:40 +0100

    added some comments for the observer pattern

--- EelData.Logger/Logger.cs
+++ EelData.Logger/Logger.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 
 namespace EelData.Logger
 {
+    /// <summary>
+    /// The Observer Design Pattern allows Observer classes to attach itself to 
+    /// this Logger class and be notified if certain events occur. 
+    /// </summary>
     public class Logger
     {
         private List<ILogObserver> _logList = new List<ILogObserver>();
 
+        /// <summary>
+        /// Attach a listening observer logging device to logger.
+        /// </summary>
+        /// <param name="observer">Observer (listening device).</param>
         public void Attach(ILogObserver observer)
         {
             _logList.Add(observer);
